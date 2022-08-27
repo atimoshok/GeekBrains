ff698396f20403282855270cab34c6f3e0f7e885
AntonTimoshok <a.timoshok@yandex.ru>
2022-08-27 16:26:08 +0700

    Innitial commit

---
+++ 2008_Seminar1/Program1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sqr = num * num;
+Console.WriteLine("Квадрат от числа " + sqr);
+
+// Console.Write("Введите число: "); // подсказка
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// // 1. int square = number * number; // number * number
+// // 2. Встроенная библиотека для математики
+// int square = Convert.ToInt32(Math.Pow(number, 2));
+// // Pow(число, степень)
+// Console.WriteLine("Квадрат от числа " + number + " = " + square); // Конкатенация
+// // Квадрат от числа 5 = 25
---
+++ 2708_DZ/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+
+if (number.Length !=5) Console.WriteLine("Число не пятизначное");
+else
+{
+    if (number[0] == number[4] && number[1] == number[3]) Console.WriteLine("да, это полиндром");
+    else Console.WriteLine("нет, это не полиндром");
+}
