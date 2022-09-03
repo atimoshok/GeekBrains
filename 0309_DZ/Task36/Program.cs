93da3ba0c795d61ba9ad825f4478433c121759c3
AntonTimoshok <a.timoshok@yandex.ru>
2022-09-03 17:35:13 +0700

    Задача №36

--- 0309_DZ/Task36/Program.cs
+++ 0309_DZ/Task36/Program.cs
@@ -10,7 +10,7 @@ for (int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().Next(-99, 100);
     Console.Write(array[i] + " ");
-    if (i % 2 == 0) sum += array[i];
+    if (i % 2 == 1) sum += array[i];
 }
 
 Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях = {sum}");
