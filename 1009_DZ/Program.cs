ed8f71225597022192b00a76fa36e470dbd26dfc
AntonTimoshok <a.timoshok@yandex.ru>
2022-09-10 13:51:37 +0700

    Задача №41

---
+++ 1009_DZ/Program.cs
@@ -0,0 +1,17 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Сколько чисел будете вводить? ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int number, count = 0;
+
+for(int i = 0; i < M; i++)
+{
+    number = Convert.ToInt32(Console.ReadLine());
+    if(number > 0) count++;
+}
+
+Console.WriteLine($"Вы ввели {count} положительных числа");
