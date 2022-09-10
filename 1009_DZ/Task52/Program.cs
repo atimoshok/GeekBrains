51076f287609f03e368336c26954511601643cb4
AntonTimoshok <a.timoshok@yandex.ru>
2022-09-10 17:09:39 +0700

    Задача №52

---
+++ 1009_DZ/Task52/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+
+void PrintMatrix(int[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int cols = matrix.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillMatrix(int rows, int cols)
+{
+    int[,] matrix = new int[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i, j] = new Random().Next(0, 100);
+        }
+    }
+    return matrix;
+}
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = FillMatrix(rows, cols);
+PrintMatrix(matrix);
+
+Console.WriteLine("Среднее арифметическое каждого столбца: ");
+for (int i = 0; i < cols; i++)
+{
+    double colsAvg = 0;
+    for (int j = 0; j < rows; j++)
+    {
+        colsAvg += matrix[j, i];
+    }
+    colsAvg = colsAvg / rows;
+    Console.Write(Math.Round(colsAvg,1) + "\t");
+}
