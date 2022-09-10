8100576974207e99588738201a91e973e78dede6
AntonTimoshok <a.timoshok@yandex.ru>
2022-09-10 16:08:44 +0700

    Задача №47

---
+++ 1009_DZ/Task47/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void PrintDoubleMatrix(double[,] matrix)
+{
+    int rows = matrix.GetLength(0);
+    int cols = matrix.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            Console.Write(Math.Round(matrix[i, j], 1) + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] FillDoubleMatrix(int rows, int cols)
+{
+    double[,] matrix = new double[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i, j] = new Random().NextDouble() * 10;
+        }
+    }
+    return matrix;
+}
+
+PrintDoubleMatrix(FillDoubleMatrix(m, n));
