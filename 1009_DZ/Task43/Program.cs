e79e3dffb4f704d301dc11f578153d91aeee194e
AntonTimoshok <a.timoshok@yandex.ru>
2022-09-10 17:18:11 +0700

    Задача №43 через массив

--- 1009_DZ/Task43/Program.cs
+++ 1009_DZ/Task43/Program.cs
@@ -6,16 +6,19 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.Write("Введите параметр b1: ");
-double b1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите параметр k1: ");
-double k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите параметр b2: ");
-double b2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите параметр k2: ");
-double k2 = Convert.ToDouble(Console.ReadLine());
+double[] arrayB = new double[2];
+double[] arrayK = new double[2];
 
-double x = -(b1 - b2) / (k1 - k2);
-double y = k1 * x + b1;
+Console.Write("Введите параметр b1: ");
+arrayB[0] = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите параметр k1: ");
+arrayK[0] = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите параметр b2: ");
+arrayB[1] = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите параметр k2: ");
+arrayK[1] = Convert.ToDouble(Console.ReadLine());
+
+double x = -(arrayB[0] - arrayB[1]) / (arrayK[0] - arrayK[1]);
+double y = arrayK[0] * x + arrayB[0];
 
 Console.WriteLine("Точка пересечения прямых: (" + Math.Round(x,1) + "; " + Math.Round(y,1) + ")");
