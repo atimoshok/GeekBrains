3facd1fe02b0117c5091faf2734b56c0b4794c7a
AntonTimoshok <a.timoshok@yandex.ru>
2022-09-18 21:51:01 +0700

    Задача №64

---
+++ 1909_DZ/Task64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+void Sequence(int start, int end)
+{
+    for (int i = start; i <= end; i++)
+    {
+        Console.Write(i + " ");
+    }
+}
+
+Console.Write("Введите науральное число начала промежутка (M): ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите науральное число конца промежутка (N): ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Sequence(M, N);
