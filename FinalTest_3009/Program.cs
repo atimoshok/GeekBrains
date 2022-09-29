b5046c252a2e792b8d02efb0d4f372ac3c16b105
AntonTimoshok <a.timoshok@yandex.ru>
2022-09-30 03:36:16 +0700

    Заполнение массива

--- FinalTest_3009/Program.cs
+++ FinalTest_3009/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых <= 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключиельно массивами.
+
+string[] FillArray(int length)
+{
+    string[] array = new string[length];
+    for(int i = 0; i < length; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+void ArrayCharCut(string[] incomeArray, int charLength)
+{
+    //string[] newArray[incomeArray.Length];
+    Console.WriteLine(incomeArray.Length);
+}
+
+Console.Write("Укажите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+string[] incomeArray = FillArray(length);
+ArrayCharCut(incomeArray, length);
