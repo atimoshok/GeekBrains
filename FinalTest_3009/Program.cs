c5aff1f2c2ed6c3eb012435c4e3c124eedbd232f
AntonTimoshok <a.timoshok@yandex.ru>
2022-09-30 04:04:43 +0700

    Программа готова!

--- FinalTest_3009/Program.cs
+++ FinalTest_3009/Program.cs
@@ -5,21 +5,47 @@
 string[] FillArray(int length)
 {
     string[] array = new string[length];
-    for(int i = 0; i < length; i++)
+    for (int i = 0; i < length; i++)
     {
+        Console.Write($"Введите {i + 1} элемент: ");
         array[i] = Console.ReadLine();
     }
     return array;
 }
 
-void ArrayCharCut(string[] incomeArray, int charLength)
+void PrintArray(string[] array)
 {
-    //string[] newArray[incomeArray.Length];
-    Console.WriteLine(incomeArray.Length);
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+string[] ArrayCharCut(string[] incomeArray, int charLength)
+{
+    int length = incomeArray.Length;
+    int j = 0;
+    string[] newArray = new string[length];
+    for (int i = 0; i < length; i++)
+    {
+        if (incomeArray[i].Length <= charLength)
+        {
+            newArray[j] = incomeArray[i];
+            j++;
+        }
+    }
+    return newArray;
 }
 
 Console.Write("Укажите длину массива: ");
 int length = Convert.ToInt32(Console.ReadLine());
 
 string[] incomeArray = FillArray(length);
-ArrayCharCut(incomeArray, length);
+Console.WriteLine("\nНачальный массив:");
+PrintArray(incomeArray);
+
+Console.Write("Укажите максимальную длину символов для элемента в новом массиве: ");
+int maxСharLength = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("\nОбработанный массив:");
+PrintArray(ArrayCharCut(incomeArray, maxСharLength));
