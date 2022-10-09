5263ce349dce6cd47d68ba072e16d11a41a6d015
AntonTimoshok <a.timoshok@yandex.ru>
2022-10-09 16:03:11 +0700

    Классная работа 09.10

--- Python/0910_Lesson2.py
+++ Python/0910_Lesson2.py
@@ -1,16 +1,36 @@
-# Напишите программу, которая принимает на вход число N и выдаёт последовательность из N членов.
-import random
+# # Напишите программу, которая принимает на вход число N и выдаёт последовательность из N членов.
+# import random
 
-N = int(input('Введите натуральнное число N: '))
-for i in range(N):
-    print(random.randint(1, 10), end = " ")
-print()
+# N = int(input('Введите натуральнное число N: '))
+# for i in range(N):
+#     print(random.randint(1, 10), end = " ")
+# print()
 
-lst = []
-for i in range(N):
-    lst.append(random.randint(1, 10))
-print(lst)
+# lst = []
+# for i in range(N):
+#     lst.append(random.randint(1, 10))
+# print(lst)
 
-# Для N = 5: 1, -3, 9, -27, 81 и т.д.
-for i in range(N):
-    print((-3)**i, end = " ")
+# # Для N = 5: 1, -3, 9, -27, 81 и т.д.
+# for i in range(N):
+#     print((-3)**i, end = " ")
+
+# 13.	Пользователь задаёт две строки. Определить количество вхождений одной строки в другой.
+
+str1 = input('Введите первую строку: ')
+str2 = input('Введите вторую строку: ')
+
+if str2 in str1:
+    print('Входит')
+else:
+    print('Такаой строки нет')
+
+count_contains = 0
+while str2 in str1:
+    count_contains += 1
+    elem_index_contain = str1.find(str2)
+    #print(str1.find(str2))
+    str1 = str1[elem_index_contain + len(str2):]
+    #print(str1)
+
+print(count_contains)
