8815ebe763bff4a78a93aabac330050b832ca530
AntonTimoshok <a.timoshok@yandex.ru>
2022-11-12 15:41:28 +0700

    Задача №3

--- Python/Homework/1111_DZ/Task3.py
+++ Python/Homework/1111_DZ/Task3.py
@@ -2,30 +2,20 @@
 
 import random
 
+
 def print_field():
     current_field = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
     for row in range(0, len(field)):
         for cal in range(0, len(field[row])):
             if field[row][cal] == 0:
                 current_field[row][cal] = '-'
-                # print(field)
-                # print(current_field)
             elif field[row][cal] == 1:
                 current_field[row][cal] = 'X'
             elif field[row][cal] == -1:
                 current_field[row][cal] = 'O'
             print(current_field[row][cal], end=' ')
         print()
-    for row in range(0, len(field)):
-        for cal in range(0, len(field[row])):
-            if field[row][cal] == 0:
-                current_field[row][cal] = '-'
-            elif field[row][cal] == 1:
-                current_field[row][cal] = 'X'
-            elif field[row][cal] == -1:
-                current_field[row][cal] = 'O'
-            print(field[row][cal], end=' ')
-        print()
+
 
 def change_player():
     if current_player == first_player:
@@ -33,26 +23,18 @@ def change_player():
     else:
         return first_player
 
+
 def if_win():
-    row_sum = 0
-    cal_sum = 0
-    for row in range(0, len(field)):
-        for cal in range(0, len(field[row])):
-            row_sum += field[row][cal]
-        if row_sum == 3 or row_sum == -3:
-            return True
-        else:
-            return False
-            break
-            
-    # for cal in range(0, len(field)):
-    #     for row in range(0, len(field[cal])):
-    #         cal_sum += field[cal][row]
-    #     if cal_sum == 3 or cal_sum == -3:
-    #         return True
-    #     else:
-    #         return False
-    #         break
+    if sum(field[0][i] for i in range(0, len(field))) == (3 or -3): return True
+    if sum(field[1][i] for i in range(0, len(field))) == (3 or -3): return True
+    if sum(field[2][i] for i in range(0, len(field))) == (3 or -3): return True
+    if sum(field[i][0] for i in range(0, len(field))) == (3 or -3): return True
+    if sum(field[i][1] for i in range(0, len(field))) == (3 or -3): return True
+    if sum(field[i][2] for i in range(0, len(field))) == (3 or -3): return True
+    if sum(field[i][i] for i in range(0, len(field))) == (3 or -3): return True
+    if sum(field[i][len(field)-1-i] for i in range(0, len(field))) == (3 or -3): return True
+    return False
+
 
 field = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 print_field()
@@ -60,15 +42,28 @@ first_player = 'X'
 second_player = 'O'
 current_player = random.choice([first_player, second_player])
 print(f'Первым ходит {current_player}')
+turn_counter = 1
 while True:
-    current_row = int(input(f'{current_player}, Укажите ряд, в который хотите поставить символ (1-3): '))
-    current_cal = int(input((f'{current_player}, Укажите столбец, в который хотите поставить символ (1-3): ')))
-    if current_player == first_player:
-        field[current_row - 1][current_cal - 1] = 1
-    else:
-        field[current_row - 1][current_cal - 1] = -1
-    print_field()
-    if if_win():
+    if turn_counter > len(field)**2:
+        print('Ничья. Победила дружба :)')
         break
-    current_player = change_player()
-print(f'Победил {current_player}')
+    else:
+        current_row = int(input(f'{current_player}, Укажите ряд, в который хотите поставить символ (1-3): '))
+        current_cal = int(input((f'{current_player}, Укажите столбец, в который хотите поставить символ (1-3): ')))
+        if current_row < 1 or current_row > len(field) or current_cal < 1 or current_cal > len(field):
+            print('[!] Вы вышли за границу поля.')
+            continue
+        if field[current_row - 1][current_cal - 1] != 0:
+            print('[!] Эта ячейка уже занята, выберите другую.')
+            continue
+        else:
+            if current_player == first_player:
+                field[current_row - 1][current_cal - 1] = 1
+            else:
+                field[current_row - 1][current_cal - 1] = -1
+            print_field()
+            if if_win():
+                print(f'Победил {current_player}')
+                break
+            current_player = change_player()
+            turn_counter += 1
